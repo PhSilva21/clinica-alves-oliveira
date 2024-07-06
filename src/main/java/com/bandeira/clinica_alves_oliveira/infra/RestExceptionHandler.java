231a2562b402e5014823226cfc29d5f10d0b43be
Pedro Bandeira <pedro.amp004@gmail.com>
2024-07-06 13:18:03 -0300

    Add ControllerAdvice

---
+++ src/main/java/com/bandeira/clinica_alves_oliveira/infra/RestExceptionHandler.java
@@ -0,0 +1,72 @@
+package com.bandeira.clinica_alves_oliveira.infra;
+
+import com.bandeira.clinica_alves_oliveira.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    private ResponseEntity<String> patientNotFound(PatientNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient not found.");
+    }
+
+    @ExceptionHandler(ProfessionalNotFoundException.class)
+    private ResponseEntity<String> professionalNotFound(ProfessionalNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professional not found.");
+    }
+
+    @ExceptionHandler(QueryNotFoundException.class)
+    private ResponseEntity<String> queryNotFound(QueryNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Query not found.");
+    }
+
+    @ExceptionHandler(ExpenseNotFoundException.class)
+    private ResponseEntity<String> expenseNotFound(ExpenseNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Expense not found.");
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    private ResponseEntity<String> accountNotFound(AccountNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Account not found.");
+    }
+
+    @ExceptionHandler(ProcedureNotFoundException.class)
+    private ResponseEntity<String> procedureNotFound(ProcedureNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Procedure not found.");
+    }
+
+    @ExceptionHandler(BudgetNotFoundException.class)
+    private ResponseEntity<String> budgetNotFound(BudgetNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Budget not found.");
+    }
+
+    @ExceptionHandler(OutstandingBalanceException.class)
+    private ResponseEntity<String> outstandigBalanceException(OutstandingBalanceException exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("The payment amount is" +
+                " greater than the patient's outstanding balance.");
+    }
+
+    @ExceptionHandler(AddressNotFoundException.class)
+    private ResponseEntity<String> addresNotFouns(AddressNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Addres not found.");
+    }
+
+    @ExceptionHandler(PaymentNotFoundException.class)
+    private ResponseEntity<String> paymentNotFound(PaymentNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Payment not found.");
+    }
+
+    @ExceptionHandler(AddressNotFoundException.class)
+    private ResponseEntity<String> addressNotFound(AddressNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Address not found.");
+    }
+
+
+}
+
